486928fc9ec168a52b83568435e59651b4f6aa9e
Davi Jorge <guimell11@hotmail.com>
2022-10-16 18:40:48 -0300

    criando pagina cadastr de endereço

---
+++ andarilho/lib/cadastroEndereco.dart
@@ -0,0 +1,244 @@
+import 'package:andarilho/config.dart';
+import 'package:andarilho/parabenizacao.dart';
+import 'package:flutter/material.dart';
+
+class CadastroEndereco extends StatefulWidget {
+  const CadastroEndereco({Key? key, title}) : super(key: key);
+
+  @override
+  State<CadastroEndereco> createState() => _CadastroEnderecoState();
+}
+
+class _CadastroEnderecoState extends State<CadastroEndereco> {
+  @override
+  Widget build(BuildContext context) {
+    AppConfig.screenSize = MediaQuery.of(context).size;
+    AppConfig.smallScreen =
+        AppConfig.screenSize.width < AppConfig.screenSize.height;
+    AppConfig.platformBrightness = MediaQuery.of(context).platformBrightness;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cadastro"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 50),
+          child: Column(
+            children: [
+              Container(
+                width: AppConfig.screenSize.width * 0.9,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: AppConfig.darkColors.onPrimary),
+                child: Column(
+                  children: [
+                    Text(
+                      "CADASTRE O ENDEREÇO DO LOCAL DO SEU SERVIÇO",
+                      style: TextStyle(
+                          color: AppConfig.darkColors.primary,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Caso seja seriço autonomo, cadastre endereço residencial",
+                      style: TextStyle(color: AppConfig.darkColors.primary),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Column(
+                        children: [
+                          const Text("Informe seu ESTADO"),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: SizedBox(
+                              width: AppConfig.screenSize.width * 0.4,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  labelText: 'Pesquisar...',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        children: [
+                          const Text("Informe sua CIDADE"),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: SizedBox(
+                              width: AppConfig.screenSize.width * 0.4,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  labelText: 'Pesquisar...',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Column(
+                        children: [
+                          const Text("Informe seu CEP"),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: SizedBox(
+                              width: AppConfig.screenSize.width * 0.4,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  labelText: 'Pesquisar...',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Column(
+                        children: [
+                          const Text("Informe sua RUA"),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 5),
+                            child: SizedBox(
+                              width: AppConfig.screenSize.width * 0.4,
+                              child: TextFormField(
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                      color: Colors.black,
+                                    ),
+                                    borderRadius: BorderRadius.circular(30.0),
+                                  ),
+                                  labelText: 'Pesquisar...',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: Column(
+                  children: [
+                    const Text("Informe sua COMPLEMENTO"),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5),
+                      child: SizedBox(
+                        width: AppConfig.screenSize.width * 0.9,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide: const BorderSide(
+                                color: Colors.black,
+                              ),
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                            labelText: 'Pesquisar...',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: SizedBox(
+                  width: AppConfig.screenSize.width * 0.8,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        primary: AppConfig.lightColors.primary),
+                    onPressed: () {},
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      child: Text(
+                        "CLIQUE PARA ENVIAR COMPROVANTE",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: SizedBox(
+                  width: AppConfig.screenSize.width * 0.4,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        primary: AppConfig.lightColors.primary),
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Parabenizacao()),
+                        (Route<dynamic> route) => false,
+                      );
+                    },
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      child: Text(
+                        "FINALIZAR",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

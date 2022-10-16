8d4e76974392921a1eb9c19c866a5b8304591686
Davi Jorge <guimell11@hotmail.com>
2022-10-16 18:41:43 -0300

    transformando historico em statefull widget

--- andarilho/lib/historico.dart
+++ andarilho/lib/historico.dart
@@ -1,25 +1,26 @@
 import 'package:andarilho/config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
 
-class Historico extends StatelessWidget {
-  final String texto;
+class Historico extends StatefulWidget {
+  const Historico({Key? key, title}) : super(key: key);
 
-  Historico(this.texto);
+  @override
+  State<Historico> createState() => _HistoricoState();
+}
 
+class _HistoricoState extends State<Historico> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(texto),
+        title: const Text("Historico"),
       ),
+      bottomNavigationBar: AppConfig.navBar(context),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              child: Text(texto),
-            )
-          ],
+          children: [],
         ),
       ),
     );

43559422f3d0e4241ce882d295f63cd3c832fa76
Davi Jorge <guimell11@hotmail.com>
2022-10-18 19:15:43 -0300

    criando pagina de perfil

--- andarilho/lib/perfil.dart
+++ andarilho/lib/perfil.dart
@@ -1,4 +1,5 @@
 import 'package:andarilho/config.dart';
+import 'package:andarilho/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -15,12 +16,72 @@ class _PerfilState extends State<Perfil> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Perfil"),
+        title: const Text("Perfil"),
       ),
       bottomNavigationBar: AppConfig.navBar(context),
       body: Center(
         child: Column(
-          children: [],
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 20, 100, 0),
+                  child: Text("Andarilha"),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: AppConfig.darkColors.primary,
+                          shape: BoxShape.circle),
+                      child: const Icon(
+                        Icons.person,
+                        size: 70,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    padding: const EdgeInsets.all(10),
+                    width: AppConfig.screenSize.width * 0.5,
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: const Text(
+                      "Maria Roberta",
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const ButtonPerfil(
+              text: "CONFIGURAÇÃO DE PERFIL",
+            ),
+            const ButtonPerfil(
+              text: "CHATS EM ABERTO",
+            ),
+            const ButtonPerfil(
+              text: "CENTRAL DE AJUDA",
+            ),
+            const ButtonPerfil(
+              text: "MINHAS AVALIAÇÕES",
+            ),
+            const ButtonPerfil(
+              text: "STATUS DE SERVIÇOS",
+            ),
+          ],
         ),
       ),
     );
